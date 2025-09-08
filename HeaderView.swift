f1a65b942e092667be2ad4c64f93831af2412c00
Alex Mitura <alexmitura61@gmail.com>
2025-09-08 11:15:18 -0500

    created a headerview then created a binding and @state, created a textfield aslong as a button

---
+++ HeaderView.swift
@@ -0,0 +1,24 @@
+
+//  HeaderView.swift
+//  GithubPractice4
+//
+//  Created by Alexander Mitura on 9/8/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    @Binding var tasks: [Task] = []
+    @State var newTask: String = ""
+    var body: some View {
+        HStack{
+            TextField("Enter a Task", text: $newTask)
+            Button("New Task") {
+                tasks += newTask
+                
+            }
+        }
+    }
+    
+}
