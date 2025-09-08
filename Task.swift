628bc666882086fadc19ddfab8b77cdcf97c75c0
Jacob C <jacobcaulfield10@gmail.com>
2025-09-08 11:04:30 -0500

    I added a struct named Task in it's own file that conforms to the Hashable protocol with the name of task variable

---
+++ Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  GithubPractice4
+//
+//  Created by Jacob M. Caulfield on 9/8/25.
+//
+import SwiftUI
+
+struct Task: Hashable {
+    var id: UUID = UUID()
+    var nameOfTask: String
+}
